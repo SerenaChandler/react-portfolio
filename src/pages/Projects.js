2a07bba59d56397c57030040f8001beca5bdb71b
Serena Chandler <serenachandler2019@gmail.com>
2021-05-23 14:12:15 -0700

    changed layout

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,34 +1,39 @@
 import react, { useState } from "react";
-import projects from "../projects.json"
-import Card from "react-bootstrap/Card"
-import Button from "react-bootstrap/Button"
+import projects from "../projects.json";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 const Projects = () => {
-
   return (
-    <section id="projects" style={{ background: "black" }}>
-
-      <h1 className="section-title" style={{ color: "grey" }}>
+    <div id="projects" >
+      
+      <h1 style={{ color: "grey", textAlign: "center" }}>
         Portfolio
       </h1>
 
+      <div className="row">
+        {projects.map((project) => (
+          <Card
+            className="col-sm-12 col-md-6 col-lg-4"
+            style={{ width: "28rem", background: "grey", margin: "auto" }}
+          >
+            <Card.Img variant="top" src={project.image} />
+            <Card.Body>
+              <Card.Title>{project.title}</Card.Title>
+              <Card.Text>{project.desc}</Card.Text>
+              <a href={project.link} target="_blank">
+                <Button style={{  background: "grey", color: "black" }} variant="primary">See Project</Button>{" "}
+              </a>
+              <a href={project.repo} target="_blank">
+                <Button style={{  background: "grey", color: "black" }} variant="primary">See Repository</Button>{" "}
+              </a>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+      
+    </div>
+  );
+};
 
-    {projects.map((project) =>  
-<Card  style={{ width: '28rem', background: "grey" }}>
-  <Card.Img variant="top" src={project.image} />
-  <Card.Body>
-    <Card.Title>{project.title}</Card.Title>
-    <Card.Text>
-      {project.desc}
-    </Card.Text>
-    <a href={project.link}target="_blank" ><Button variant="primary" >See Project</Button> </a>
-   <a href={project.repo}target="_blank" ><Button variant="primary">See Repository</Button> </a> 
-  </Card.Body>
-</Card>
-    )}
-
-    </section>
-  )
-}
-
-export default Projects
+export default Projects;
