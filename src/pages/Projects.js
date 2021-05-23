d58d58b9da5ab7a2a8ab7919f1fb43810d6dcc1a
Serena Chandler <serenachandler2019@gmail.com>
2021-05-23 13:20:33 -0700

    created a Card that maps through Json

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,2 +1,26 @@
 import react, { useState } from "react";
 import projects from "../projects.json"
+import Card from "react-bootstrap/Card"
+import Button from "react-bootstrap/Button"
+
+const Projects = () => {
+
+  return (
+
+    projects.map((project) => 
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>{project.title}</Card.Title>
+    <Card.Text>
+      {project.desc}
+    </Card.Text>
+    <a href={project.link}target="_blank" ><Button variant="primary" >See Project</Button> </a>
+   <a href={project.repo}target="_blank" ><Button variant="primary">See Repository</Button> </a> 
+  </Card.Body>
+</Card>
+    )
+  )
+}
+
+export default Projects
