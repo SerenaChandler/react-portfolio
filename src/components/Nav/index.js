1cef8f41cd1c981f9a6c897c27b71f15bd0a6a08
Serena Chandler <serenachandler2019@gmail.com>
2021-05-25 09:23:06 -0700

    adjusted navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,28 +4,28 @@ import React from "react";
 function Nav() {
   return (
     <div className="text-center fixed-top" style={{ background: "#8a0000", paddingTop: 10, paddingBottom: 25, }}>
-      <nav className="">
-        <h1 className="d-inline mx-4 nav-item">
+      <nav className="row">
+        <h1 className="col mx-4 nav-item">
           <a style={{ color: "white" }} href="#home" className="text-decoration-none">
             Home 
           </a>
         </h1>
-        <h1 className="d-inline mx-4 nav-item">
+        <h1 className="col mx-4 nav-item">
           <a style={{ color: "white" }} href="#about" className="text-decoration-none">
             About 
           </a>
         </h1>
-        <h1 className="d-inline mx-4 nav-item">
+        <h1 className="col mx-4 nav-item">
           <a style={{ color: "white" }} href="#projects" className="text-decoration-none">
             Projects
           </a>
         </h1>
-        <h1 className="d-inline mx-4 nav-item">
+        <h1 className="col mx-4 nav-item">
           <a style={{ color: "white" }} href="#resume" className="text-decoration-none">
             Resume
           </a>
         </h1>
-        <h1 className="d-inline mx-4 nav-item">
+        <h1 className="col mx-4 nav-item">
           <a style={{ color: "white" }} href="#contact" className="text-decoration-none">
             Contact
           </a>
